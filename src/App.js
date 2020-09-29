f9c5f4b12ba8439bed38880927f1424e891ab09d
patrick <patrick204nqh@gmail.com>
2020-09-30 00:34:45 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = (props) => {
+  const [people, setPeople] = useState([
+    {
+      firstName: 'patrick',
+      lastName: 'karl'
+    },
+    {
+      firstName: 'robert',
+      lastName: 'silva'
+    }
+  ])
+
+  const [person, setPerson] = useState({ firstName: '', lastName: '' })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (person.firstName.trim() === '' || person.lastName.trim() === '') return
+
+    const newPerson = {
+      firstName: person.firstName.trim(),
+      lastName: person.lastName.trim()
+    }
+
+    setPeople([...people, newPerson])
+    setPerson({ firstName: '', lastName: '' })
+  }
+
+  const onChange = (e) => {
+    setPerson({ ...person, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>hello, world</div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>Add a person</h2>
+          <hr />
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input type="text" className="form-control"
+                name="firstName"
+                placeholder="First Name.."
+                value={person.firstName}
+                onChange={onChange} />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control"
+                name="lastName"
+                placeholder="Last Name.."
+                value={person.lastName}
+                onChange={onChange} />
+            </div>
+            <button className="btn btn-success" type="submit">Add person</button>
+          </form>
+        </div>
+        <div className="col">
+          <h2>People: </h2>
+          <hr />
+          {people.map((p, index) => (
+            <div key={index}>
+              <p>{p.firstName} {p.lastName}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
